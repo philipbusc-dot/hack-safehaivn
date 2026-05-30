f3726f648e844e7584f01f956ad067e3c80f66fc
philipbusc-dot <philip.busc@mail.kmutt.ac.th>
2026-05-30 09:11:19 +0700

    832 - [ADD] Added error handler for the backend

--- backend/src/modules/addition/routers/addition.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { calculate, getHistory } from "../controllers/addition.controller";
-
-const additionRouter = Router();
-
-additionRouter.post("/", calculate);
-additionRouter.get("/history", getHistory);
-
-export default additionRouter;
--- backend/src/modules/multiplication/routers/multiplication.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { calculate, getHistory } from "../controllers/multiplication.controller";
-
-const multiplicationRouter = Router();
-
-multiplicationRouter.post("/", calculate);
-multiplicationRouter.get("/history", getHistory);
-
-export default multiplicationRouter;
