fc6e1b591f40999cdb2c4031e788dab2f2a20e45
Ronald-875 <ronaldkok@duck.com>
2026-05-30 16:52:36 +0700

    Merged

--- frontend/src/routers.tsx
+++ frontend/src/routers.tsx
@@ -1,19 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
 import { connectRoutes } from "./modules/connect/routers/connect.router";
 import { countryReportRoutes } from "./modules/countryReport/routers/countryReport.router";
 import { mapRoutes } from "./modules/map/routers/map.router";
 import { profileRoutes } from "./modules/profile/routers/profile.router";
+import { RiskRouter } from "./modules/riskScore/routers/risk.router";
 
 const mainRouter = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      ...connectRoute,
-      [...RiskRouter,
+      ...connectRoutes,
+      ...RiskRouter,
       ...countryReportRoutes,
       ...mapRoutes,
       ...profileRoutes,
