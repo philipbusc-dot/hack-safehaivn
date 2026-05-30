de71e7804a376e457bc3e7665ad4cc67345e3d2e
Ronald-875 <ronaldkok@duck.com>
2026-05-30 13:54:18 +0700

    875 - [Update] Updated App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <h1>Welcome to Shared Folder Template!</h1>
+    <div className="w-screen h-screen bg-bg flex items-center justify-center">
       <Outlet />
     </div>
   );
